8ea00fc89ebdc9fa762608c11f1a93cb2cbabb8e
Pedro Sampaio <henripedro652@gmail.com>
2026-04-15 10:49:12 -0300

    Update conjuntos.py

--- Python/revendo conceitos/conjuntos.py
+++ Python/revendo conceitos/conjuntos.py
@@ -11,3 +11,30 @@ print(carros)
 
 linguagens = {"python", "java", "python"} #pode ser ativo por chaves o set, não precisando declarar o mesmo como: set({}) e usando só {}
 print(linguagens)
+
+for indice, linguagem in enumerate(linguagens):
+    print(f"indice: {indice}, linguagem: {linguagem}")
+
+for linguagem in linguagens:
+    print(linguagem)
+
+
+
+
+"""
+linguagens = list(linguagens)
+print(linguagens[0]
+"""
+
+
+
+
+"""
+O Python usa chaves { } para duas estruturas diferentes. O que define qual será criada é o que você coloca dentro delas:
+
+Dicionário: Se houver pares de chave: valor (ex: {"nome": "Python"}).
+
+Set (Conjunto): Se houver apenas valores soltos (ex: {"python", "java"}).
+
+conjuntos não fazem fatiamente e nem indexação por issp é complicado pedir algo em especifico, somente convertendo em lista
+"""
