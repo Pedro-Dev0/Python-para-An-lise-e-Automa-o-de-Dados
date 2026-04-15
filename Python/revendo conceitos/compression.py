ea2bcde8f27d4ee32a6c5aa2a5b30177416d2b8d
Pedro Sampaio <henripedro652@gmail.com>
2026-04-15 10:23:16 -0300

    revendo conceitos

--- Python/revendo conceitos/compression.py
+++ Python/revendo conceitos/compression.py
@@ -2,12 +2,17 @@ numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20
 pares = [numero for numero in numeros if numero % 2 == 0] #compression para fazer code menor
 impares = [numero for numero in numeros if numero % 2 != 0]
 quadrado = [numero ** 2 for numero in numeros]
+dobro = [numero * 2 for numero in numeros if numero > 9]
+dobro_par = [numero for numero in numeros if numero >= 20 and numero % 2 == 0]
 
 #  esse != sinal é de desigualdade e é oposto ao de igualdade o ==
 print(pares)
 print(impares)
 print(numeros)
 print(quadrado)
+print(dobro)
+print(dobro_par)
+
 
 for indice, numero in enumerate(numeros):
     print(f"indice: {indice}, numero: {numero}")
