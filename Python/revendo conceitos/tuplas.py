284180adbc27074389344f6db5c649080b2df779
Pedro Sampaio <henripedro652@gmail.com>
2026-04-15 10:28:40 -0300

    Update tuplas.py

---
+++ Python/revendo conceitos/tuplas.py
@@ -0,0 +1,22 @@
+frutas = ("laranja", "pera", "uva",)
+# sempre tem que ter a virgula no final dos parentes para o python entender que é tupla
+print(frutas)
+
+letras = tuple("python")
+print(letras)
+# colocado o objeto tuple para que possa entender e tranformar as letras 1 por uma e tuplas como se fosse uma lista mas imutavel
+
+numeros = tuple([1, 2, 3, 4, 5])
+print(numeros)
+# novamente passado assim para reconhecer como tupla pois numeros só são reconhecidos em colchetes já que a tupla aceita  melhor string
+
+
+numero_tupla = (1, 2, 3, 4, 5,)# assim também aceita
+print(numero_tupla)
+print(type(numero_tupla))
+
+pais = ("Brasil",)
+print(pais)
+print(type(pais))
+
+
