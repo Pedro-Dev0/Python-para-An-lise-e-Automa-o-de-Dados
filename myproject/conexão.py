f1c52520e9470db7120125e5b30ee351c8ac9138
PedroSampaio <henripedro652@gmail.com>
2026-06-06 21:47:39 -0300

    Update conexão.py

--- myproject/conexão.py
+++ myproject/conexão.py
@@ -3,5 +3,11 @@ from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
 
-conexao = sqlite3.connect('clientes.db')
+conexao = sqlite3.connect(ROOT_PATH / 'clientes.db')
 print(conexao)
+
+cursor = conexao.cursor()
+print(cursor)
+
+cursor.execute('CREATE TABLE IF NOT EXISTS clientes(id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR(50) NOT NULL, email VARCHAR(100) NOT NULL UNIQUE)')
+
