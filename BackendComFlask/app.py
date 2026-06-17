02e1ed1df94531246fdc3954e5d0f1adcfa16259
PedroSampaio <henripedro652@gmail.com>
2026-06-16 22:35:07 -0300

    flask

--- BackendComFlask/app.py
+++ BackendComFlask/app.py
@@ -2,8 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 # coloque poetry env info no terminal para colocar interpretador o env criado
-@app.route("/")
+
+@app.route("/") # rota só com / é porque não tem caminho feito, seria como ir para home, demais rotas para locais especificos
+ 
 def hello_world():
     return "<p>Hello, World!</p>"
-# para rodar use no terminal ' poetry run flask --app app run para ativar o ambiente virtual
+# para rodar use no terminal ' poetry run flask --app app run para ativar o ambiente virtual / poetry run flask --app app run --debug (modo debug)
 
