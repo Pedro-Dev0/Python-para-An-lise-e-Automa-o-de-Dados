563ef18b91aa199eff0fa077b22b02ac863b0185
PedroSampaio <henripedro652@gmail.com>
2026-06-16 22:06:37 -0300

    flask

--- BackendComFlask/app.py
+++ BackendComFlask/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+# coloque poetry env info no terminal para colocar interpretador o env criado
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+# para rodar use no terminal ' poetry run flask --app app run para ativar o ambiente virtual
 
